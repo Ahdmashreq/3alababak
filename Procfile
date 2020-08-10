9fcc9452a79a99a92392aade514f084a2df17a64
ahdhozayen <ahdhozayen@gmail.com>
2020-08-10 15:38:16 +0200

    last updated development in leave and attendance

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn alababak.wsgi
--- alababak/settings.py
+++ alababak/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -160,3 +160,7 @@ CACHES = {
 }
 CITIES_LIGHT_TRANSLATION_LANGUAGES = ['ar', 'en']
 CITIES_LIGHT_INCLUDE_COUNTRIES = ['EG','UAE','SA']
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
